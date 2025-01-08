917447fc0dbb4c5172b30c9d19d6197a4a59d54d
Serge-Mane <sergemane81@gmail.com>
2025-01-08 16:26:27 +0000

    le repository c'est lui qui permet d'ecrire dans la base de donnees apres sa creation il sera appeller dans le service

---
+++ src/main/java/tech/sam/ms_naissances/profiles/ProfilesRepository.java
@@ -0,0 +1,6 @@
+package tech.sam.ms_naissances.profiles;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ProfilesRepository extends JpaRepository<Profile,Integer> {
+}
--- src/main/java/tech/sam/ms_naissances/profiles/ProfilesServicce.java
+++ src/main/java/tech/sam/ms_naissances/profiles/ProfilesServicce.java
@@ -7,9 +7,15 @@ import org.springframework.web.bind.annotation.RequestBody;
 
 @Service
 public class ProfilesServicce {
+    private final ProfilesRepository profilesRepository;
     Logger logger= LoggerFactory.getLogger(ProfilesControleur.class);
+
+    public ProfilesServicce(ProfilesRepository profilesRepository) {
+        this.profilesRepository = profilesRepository;
+    }
 
     public void create(Profile profile){
         logger.info("Nouveau  compte  avec l'email {}", profile.getEmail());
+        this.profilesRepository.save(profile);
     }
 }
