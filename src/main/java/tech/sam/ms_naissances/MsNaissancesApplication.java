9117e9a4fd9122d891ac8f3422c0ef24d66b2e91
Serge-Mane <sergemane81@gmail.com>
2025-01-29 12:30:11 +0000

    generation des cles privees et public apartir d'une cle generale

--- src/main/java/tech/sam/ms_naissances/MsNaissancesApplication.java
+++ src/main/java/tech/sam/ms_naissances/MsNaissancesApplication.java
@@ -3,11 +3,11 @@ package tech.sam.ms_naissances;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.actuate.autoconfigure.security.servlet.ManagementWebSecurityAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceTransactionManagerAutoConfiguration;
-import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import tech.sam.ms_naissances.security.RsaKeys;
 
+@EnableConfigurationProperties(RsaKeys.class)
 @SpringBootApplication(
 		exclude={
 				SecurityAutoConfiguration.class,
